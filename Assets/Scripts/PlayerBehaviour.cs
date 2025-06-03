65cfbeeb007a509b7cc957923552c44db780882d
trongphuc <phucnhtps39905@gmail.com>
2025-06-03 11:19:22 +0700

    Map1

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,54 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == "Map 1")
+        {
+            HandleMap1();
+        }
+    }
+
+    void HandleMap1()
+    {
+        // Di chuy?n t? ??ng sang ph?i, gi? y (tr?c d?c) hi?n t?i
+        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+
+        // Nh?y khi nh?n chu?t tr�i v� ?ang ch?m ??t
+        if (Input.GetMouseButtonDown(0) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false; // Ch?n nh?y li�n t?c
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
