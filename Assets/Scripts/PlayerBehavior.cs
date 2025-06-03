8e472f865a70924fe762d56057bf451e5c53c527
Sobek <congtientran041@gmail.com>
2025-06-03 11:50:30 +0700

    map1neww

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -17,14 +17,10 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
         string scene = SceneManager.GetActiveScene().name;
-
-        // Di chuyển liên tục theo trục X
         rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
-
-        // Nhảy khi nhấn chuột và đang đứng trên mặt đất
         if (Input.GetMouseButtonDown(0) && isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0); // Reset vận tốc Y trước khi nhảy
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
         }
@@ -32,7 +28,6 @@ public class PlayerBehavior : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Kiểm tra va chạm từ phía dưới (mặt đất)
         foreach (ContactPoint2D contact in collision.contacts)
         {
             if (contact.normal.y > 0.5f)
